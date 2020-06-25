756b681d818942f5a6c072cf738ef528f29a409d
Slewog <31140215+Slewog@users.noreply.github.com>
2020-06-25 02:49:21 +0200

    Addition of a welcome screen, life bar and armor for the player

--- main.py
+++ main.py
@@ -3,14 +3,24 @@
 
 import os  # Import of python modules.
 import sys
+from math import ceil
 import pygame
 from game import Game
 
-os.environ['SDL_VIDEO_WINDOW_POS'] = "150,150"
+os.environ['SDL_VIDEO_WINDOW_POS'] = "250,150"
 pygame.init()  # Generate the game window.
 pygame.display.set_caption("Shooter")  # Window title.
 game_window = pygame.display.set_mode((1400, 800))  # Window size.
-background = pygame.image.load('assets/background.jpg')  # Window logo.
+background = pygame.image.load('assets/background.jpg')  # Import window logo.
+banner = pygame.image.load('assets/banner.png')  # Import banner.
+banner = pygame.transform.scale(banner, (600, 600))
+banner_rect = banner.get_rect()
+banner_rect.x = ceil(game_window.get_width() / 3.5)
+button_play = pygame.image.load('assets/button.png')  # Import button play.
+button_play = pygame.transform.scale(button_play, (575, 250))
+button_play_rect = button_play.get_rect()
+button_play_rect.x = ceil(game_window.get_width() / 3.3)
+button_play_rect.y = ceil(game_window.get_height() / 1.9)
 if sys.platform == 'win32':
     pygame.display.set_icon(pygame.image.load("assets/logo.png"))  # Logo application if windows operating system.
 
@@ -20,29 +30,13 @@ games_running = True  # Enables you to enter the game loop.
 # Game loop.
 while games_running:
     game_window.blit(background, (0, -130))  # Apply the background image on the game window.
-    game_window.blit(game.player.image, game.player.rect)  # Apply player image.
-    game.player.update_health_bar(game_window)  # Displays the life bar of the player.
-    game.player.update_armor_bar(game_window)  # Displays the armor bar of the player.
 
-    # Collect the projectiles.
-    for projectile in game.player.all_projectiles:
-        projectile.move_bullets()  # Moves projectiles on the window.
-
-    # Collect the monsters.
-    for monster in game.all_monsters:
-        monster.forward()  # Move group of monsters.
-        monster.update_health_bar(game_window)  # Displays the life bar of the monsters.
-
-    game.player.all_projectiles.draw(game_window)  # Apply player's projectile.
-
-    game.all_monsters.draw(game_window)  # Apply monsters group.
-
-    # If the key pressed is equal to the right and the position smaller than the window size.
-    if game.pressed.get(pygame.K_RIGHT) and game.player.rect.x + game.player.rect.width < game_window.get_width():
-        game.player.move_right()  # Moves the player to the right.
-    # If the key pressed is equal to the left and the position greater than 0 pixels.
-    elif game.pressed.get(pygame.K_LEFT) and game.player.rect.x > 0:
-        game.player.move_left()  # Moves the player to the left.
+    # Check if the game to start.
+    if game.is_playing:
+        game.update_game()
+    else:  # Otherwise display the home screen
+        game_window.blit(button_play, button_play_rect)  # Displays the play button_play.
+        game_window.blit(banner, banner_rect)  # Displays the banner.
 
     pygame.display.flip()  # Update the game window.
 
@@ -61,3 +55,7 @@ while games_running:
         # Check if a key is released.
         elif event.type == pygame.KEYUP:
             game.pressed[event.key] = False  # Stores the released key in a dictionary to the value FALSE.
+        # Check if a mouse button_play is pressed.
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if button_play_rect.collidepoint(event.pos):
+                game.start_game()
